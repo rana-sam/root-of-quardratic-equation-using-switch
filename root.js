4f4c82cbe8cd006a4be5080233bc2d9b5821ae3b
Muhammad Usama <82697108+rana-sam@users.noreply.github.com>
2021-04-24 14:23:54 +0500

    done

--- root.js
+++ root.js
@@ -2,18 +2,17 @@ let a=parseInt(prompt("Enter first(a) value"))
 let b=parseInt(prompt("Enter second(b) value"))
 let c=parseInt(prompt("Enter third(c) value"))
 let dis=(b*b)-(4*a*c)
-let disF=(dis)^(1/2)
 switch (true) {
     case (dis>0):
-        let root1=(-b+ disF)/(2*a)
-        let root2=(-b- disF)/(2*a)
+        let root1=(-b+ Math.sqrt(dis))/(2*a)
+        let root2=(-b- Math.sqrt(dis))/(2*a)
         console.log("first root is: "+root1);
         console.log("second root is: "+root2);
         break;
     case (dis<0):
         let real=-b/(2*a)
         console.log("real part is"+real);
-        let imag=disF/(2*a);
+        let imag=Math.sqrt(-dis)/(2*a);
         console.log("imaginary part is "+imag);
         break;
     case(dis==0):
